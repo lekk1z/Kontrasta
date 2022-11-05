ea80fe85b47a4e3adb1d3d9e49f573e1ab5fd205
Петар Родић <116944957+trinstone@users.noreply.github.com>
2022-11-05 11:03:20 +0000

    Dodata metoda za kontrast

--- Main.cs
+++ Main.cs
@@ -14,8 +14,32 @@ public class Program
   {
     
   }
-  static void Kontrast(Bitmap slika)
+  
+  static void Kontrast(ref Bitmap slika,double procenat)
   {
+double contrast,factor;
+contrast=(procenat*255)/100;
+if(procenat==0)return;
+factor = (259 * (contrast + 255)) / (255 * (259 - contrast));
+for (int i = 0; i <img.Height; i++)
+{
+  for (int j = 0; j < img.Width; j++)
+  {
+   Color pixel = slika.GetPixel(j,i);
+    int red=Convert.ToInt32(Math.Truncate(factor * (pixel.R - 128) + 128));
+    int green=Convert.ToInt32(Math.Truncate(factor * (pixel.G - 128) + 128));
+    int blue=Convert.ToInt32(Math.Truncate(factor * (pixel.B - 128) + 128));
+    if(red>255)red=255;
+    if(green>255)green=255;
+    if(blue>255)blue=255;
+    if(red<0)red=0;
+    if(green<0)green=0;
+    if(blue<0)blue=0;
+    Color novo=Color.FromArgb(red,green,blue);
+    slika.SetPixel(j,i,novo);
+    slika.Save("menjano.bmp");//Aleksa stavi naziv promenjene
+  }
+}
     
   }
   public static void Main(string[] args) 
